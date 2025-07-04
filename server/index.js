cd3de9d8ae0d0b64d2de6afedfb8530cefa20447
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-07-04 23:30:18 +0530

    added connectdb code and installed backend libraries

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors"
+//import api from "./routes/api.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+
+//app.use("/api", api);
+//app.use('/uploads', express.static('uploads'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
