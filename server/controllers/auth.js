bd6de276a890a12ceb87887c51e9b23e64096e8b
angad2212 <angadsuryavanshi22@gmail.com>
2025-09-15 15:55:15 +0530

    added rough auth page structure

---
+++ server/controllers/auth.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/user.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+//register
+
+//login
+
+//get profile
+
+//update profile
+
+export default router;
